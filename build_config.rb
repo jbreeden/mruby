e970bdf7b5a2e730234f8dceae1ffcba5ede1770
jbreeden <jared.breeden@gmail.com>
2015-08-09 12:09:39 -0700

    Set lamina build_config settings

--- build_config.rb
+++ build_config.rb
@@ -1,14 +1,51 @@
+# Thanks! http://stackoverflow.com/questions/170956/how-can-i-find-which-operating-system-my-ruby-program-is-running-on
+module OS
+  def OS.windows?
+    (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  end
+
+  def OS.mac?
+   (/darwin/ =~ RUBY_PLATFORM) != nil
+  end
+
+  def OS.unix?
+    !OS.windows?
+  end
+
+  def OS.linux?
+    OS.unix? and not OS.mac?
+  end
+end
+
 MRuby::Build.new('host') do |conf|
   # Gets set by the VS command prompts.
   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
     toolchain :visualcpp
   else
-    toolchain :gcc
+    toolchain :clang
   end
 
-  conf.gembox "full-core"
+  if OS.mac?
+    conf.cc.flags << '-m64'
+    conf.cxx.flags << '-m64'
+    conf.cc.flags << '-stdlib=libstdc++'
+    conf.cxx.flags << '-stdlib=libstdc++'
+    conf.linker.flags << '-stdlib=libstdc++'
+  end
 
+  Dir.entries('mrbgems').reject { |f| f =~ /bin/ || !(f =~ /mruby/) }.each do |gem|
+    conf.gem core: gem
+  end
+
+  conf.gem :github => 'iij/mruby-regexp-pcre'
+  conf.gem :github => 'iij/mruby-dir'
+  conf.gem :github => 'iij/mruby-errno'
+
+  conf.gem "../mrbgems/mruby-lamina"
+  conf.gem "../mrbgems/mruby-bin-lamina"
+  conf.gem "../mrbgems/mruby-cef"
   conf.gem "../mrbgems/mruby-apr"
+  configure_mruby_apr(conf)
 
   if ENV['DEBUG'] && (ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR'])
     conf.cc.flags << "/DEBUG"
