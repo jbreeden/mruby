65e0c4320119d9ecd677bf07cc17d4455243f6ac
jbreeden <jared.breeden@gmail.com>
2015-08-12 08:56:09 -0700

    Adding build for console testing

--- build_config.rb
+++ build_config.rb
@@ -43,9 +43,40 @@ MRuby::Build.new('host') do |conf|
 
   conf.gem "../mrbgems/mruby-apr"
   configure_mruby_apr(conf)
-  conf.gem "../mrbgems/mruby-lamina"
-  conf.gem "../mrbgems/mruby-bin-lamina"
+  conf.gem "../mrbgems/mruby-rubium"
+  conf.gem "../mrbgems/mruby-bin-rubium"
   conf.gem "../mrbgems/mruby-cef"
+
+  if ENV['DEBUG'] && (ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR'])
+    conf.cc.flags << "/DEBUG"
+    conf.cxx.flags << "/DEBUG"
+  end
+
+end
+
+MRuby::Build.new('console') do |conf|
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :clang
+  end
+
+  if OS.mac?
+    conf.cc.flags << '-m64'
+    conf.cxx.flags << '-m64'
+    conf.cc.flags << '-stdlib=libstdc++'
+    conf.cxx.flags << '-stdlib=libstdc++'
+    conf.linker.flags << '-stdlib=libstdc++'
+  end
+
+  conf.gembox 'full-core'
+
+  conf.gem :github => 'iij/mruby-regexp-pcre'
+  conf.gem :github => 'iij/mruby-dir'
+  conf.gem :github => 'iij/mruby-errno'
+
+  conf.gem "../mrbgems/mruby-apr"
 
   if ENV['DEBUG'] && (ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR'])
     conf.cc.flags << "/DEBUG"
