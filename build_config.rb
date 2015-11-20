ce414fd605f79376f891fe5a1ad06d32fadce097
jbreeden <jared.breeden@gmail.com>
2015-11-19 19:55:17 -0800

    Updating build_config.rb

--- build_config.rb
+++ build_config.rb
@@ -7,21 +7,20 @@ MRuby::Build.new do |conf|
   end
 
   conf.gembox 'full-core'
+  # This should point to where you downloaded mruby-curses
   conf.gem '../mruby-curses'
   conf.gem github: 'jbreeden/mruby-apr'
   conf.gem github: 'jbreeden/mruby-sqlite'
 
   # Linker settings
   conf.linker do |linker|
-    # These just happen to be the paths I used on my system.
-    # You'll want to update these before building.
     if /windows/i =~ ENV['OS']
       linker.libraries = %w(pdcurses)
     else
       linker.libraries = %w(panel ncurses)
     end
 
-    # These just happen to be the paths I used on my system.
+    # These paths are just where I happened to build these libraries.
     # You'll want to update these before building.
     if /windows/i =~ ENV['OS']
       linker.library_paths = ['./pdcurses/win32']
